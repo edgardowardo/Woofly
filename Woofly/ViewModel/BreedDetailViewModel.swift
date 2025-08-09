e9768faed6c319ddd9abba9725bc2002b4120200
Edgar Agno <edgardo76@yahoo.com>
2025-08-09 17:25:18 +0100

    Add API to Breed Detail View Model

--- Woofly/ViewModel/BreedDetailViewModel.swift
+++ Woofly/ViewModel/BreedDetailViewModel.swift
@@ -6,8 +6,20 @@ class BreedDetailViewModel: ObservableObject, BreedDisplayProviding {
     @Published var imageUrls: [URL]
     let breed: DogBreed
     
-    init(breed: DogBreed) {
+    private var api: DogAPIProviding
+    
+    init(breed: DogBreed, api: DogAPIProviding = DogAPI()) {
         self.breed = breed
+        self.api = api
         imageUrls = []
+    }
+    
+    func fetch() async throws {
+        do {
+            imageUrls = try await api.fetchImages(from: breed, count: 10)
+        } catch {
+            imageUrls = []
+        }
+
     }
 }
