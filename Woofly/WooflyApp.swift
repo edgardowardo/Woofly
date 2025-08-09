28747bef3e9663d4477da6aa8e3c4ef5c4665f29
Edgar Agno <edgardo.agno@gmail.com>
2025-08-09 10:51:28 +0100

    Initial Commit

---
+++ Woofly/WooflyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WooflyApp.swift
+//  Woofly
+//
+//  Created by EDGARDO AGNO on 09/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct WooflyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WooflyTests/WooflyTests.swift
@@ -0,0 +1,17 @@
+//
+//  WooflyTests.swift
+//  WooflyTests
+//
+//  Created by EDGARDO AGNO on 09/08/2025.
+//
+
+import Testing
+@testable import Woofly
+
+struct WooflyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
